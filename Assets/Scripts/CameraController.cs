dcf6c3acd0976e7918c7c4945b4aba9af5e3e247
LueptowK <kylelueptow@gmail.com>
2020-02-01 12:46:01 -0600

    Camera functionality

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,15 +7,22 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform cam;
     [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float distance;
+    [SerializeField] private float height;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
         cam.LookAt(player);
+        Cursor.lockState = CursorLockMode.Locked;
+        offset = new Vector3(player.position.x, player.position.y + height, player.position.z + distance);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void LateUpdate()
     {
-        transform.Translate(Input.GetAxis("Mouse X") * cam.right * horizontalSpeed * Time.deltaTime);
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * horizontalSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
     }
 }
