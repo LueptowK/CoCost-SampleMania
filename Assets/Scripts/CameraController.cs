42e8a4925cbdf0f0e933bfbf978fd4f0a40062ae
LueptowK <kylelueptow@gmail.com>
2020-02-01 12:24:45 -0600

    Controller stuff, camera stuff

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float horizontalSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.LookAt(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Mouse X") * cam.right * horizontalSpeed * Time.deltaTime);
+    }
+}
