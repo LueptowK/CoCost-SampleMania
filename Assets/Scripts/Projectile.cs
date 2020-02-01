e85b1b08c90cf98968d1dd45580fed93cb11f58a
williamwolf11 <wwolf@middlebury.edu>
2020-02-01 15:34:18 -0600

    part 2

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    Transform tf;
+    Rigidbody rb;
+    [SerializeField] protected Transform target;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.magnitude > 3000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Fire(Vector3 direction, float speed)
+    {
+        rb.velocity = direction * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform e = other.GetComponent<Transform>();
+        if (e == target)
+        {
+            Destroy(gameObject);
+        }
     }
 }
