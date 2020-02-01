4514194f253db7b78f55debd8abc1d2f0dbbed51
williamwolf11 <wwolf@middlebury.edu>
2020-02-01 14:51:43 -0600

    changes to projectile and turret

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,20 +11,23 @@ public class EnemyController : MonoBehaviour
     protected float maxRepairAmount = 100;
     protected float repairAmount;
     protected float repairTime;
+    protected float startFireTime = 1.5f;
+    protected float fireTime;
+    [SerializeField] protected GameObject projectilePrefab;
 
     // Start is called before the first frame update
     protected virtual void Start()
     {
+        fireTime = startFireTime;
         rb = GetComponent<Rigidbody>();
         tf = GetComponent<Transform>();
         repairAmount = maxRepairAmount;
-
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-
+        
     }
 
     public virtual bool Repair(float repairValue)
@@ -36,5 +39,18 @@ public class EnemyController : MonoBehaviour
             isRepaired = true;
         }
         return isRepaired;
+    }
+
+    protected virtual void Fire()
+    {
+        Vector3 lookDirection = new Vector3(1, 0, 0);
+        if (fireTime <= 0){
+            fireTime = startFireTime;
+            GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Vector3.up * 0.5f, Quaternion.identity);
+
+            Projectile projectile = projectileObject.GetComponent<Projectile>();
+            projectile.Fire(lookDirection, 300.0f);
+            Debug.Log("Fired:)");
+        }
     }
 }
--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -7,13 +7,17 @@ public class TurretController : EnemyController
     // Update is called once per frame
     protected override void Update()
     {
-
+        fireTime -= Time.deltaTime;
+        //transform.LookAt(target);
+        //Fire();
         if (isRepaired == true)
         {
             repairTime += Time.deltaTime;
             if (repairTime >= 2.0f)
             {
                 transform.LookAt(target);
+                Fire();
+                Debug.Log("Fired!!!");
             }
         }
         
