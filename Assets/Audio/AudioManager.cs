87f7950b78e68273b6063e164d715db05e348eef
caseyjarmes <44095299+caseyjarmes@users.noreply.github.com>
2020-02-01 21:46:58 -0600

    Summary 9:46
    Added sound
    Replaced shelves with updated models
    Placed turrets

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] paClips = new AudioClip[56];
+    private AudioSource audioManager;
+    private int N;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = GetComponent<AudioSource>();
+        InvokeRepeating("PlaySound", 2.0f, 4.0f);
+    }
+
+    void PlaySound()
+    {
+        N = Random.Range(0, paClips.Length);
+        if (!audioManager.isPlaying)
+        {
+            audioManager.PlayOneShot(paClips[N], 1.0f);
+        }
+    }
+
+}
